fc1738af97c8624da86e2c435cc1f0a62b00779a
David Echo <echohack@users.noreply.github.com>
2019-10-20 11:28:02 -0700

    Add CEPGP configuration
    Signed-off-by: David Echo <echohack@users.noreply.github.com>

---
+++ CEPGP.lua
@@ -0,0 +1,843 @@
+
+CHANNEL = "GUILD"
+COEF = 1
+MOD_COEF = 1
+MOD = 1
+AUTOEP = {
+	["High Priestess Jeklik"] = false,
+	["Gothik the Harvester"] = false,
+	["Buru the Gorger"] = false,
+	["Wushoolay"] = false,
+	["Lord Kri"] = false,
+	["Emperor Vek'nilash"] = false,
+	["Ossirian the Unscarred"] = false,
+	["Golemagg the Incinerator"] = false,
+	["Grand Widow Faerlina"] = false,
+	["High Priestess Mar'li"] = false,
+	["Majordomo Executus"] = false,
+	["High Priest Venoxis"] = false,
+	["Gehennas"] = false,
+	["Taerar"] = false,
+	["Ysondre"] = false,
+	["Thane Korth'azz"] = false,
+	["High Priest Thekal"] = false,
+	["Noth the Plaguebringer"] = false,
+	["Lethon"] = false,
+	["Moam"] = false,
+	["The Prophet Skeram"] = false,
+	["Flamegor"] = false,
+	["C'Thun"] = false,
+	["General Rajaxx"] = false,
+	["Princess Huhuran"] = false,
+	["Thaddius"] = false,
+	["Loatheb"] = false,
+	["Lady Blaumeux"] = false,
+	["Highlord Mograine"] = false,
+	["Heigan the Unclean"] = false,
+	["Azuregos"] = false,
+	["Doom Lord Kazzak"] = false,
+	["Teremus the Devourer"] = false,
+	["Chromaggus"] = false,
+	["Baron Geddon"] = false,
+	["Firemaw"] = false,
+	["Sulfuron Harbinger"] = false,
+	["Gri'lek"] = false,
+	["High Priestess Arlokk"] = false,
+	["Emperor Vek'lor"] = false,
+	["Gahz'ranka"] = false,
+	["Gluth"] = false,
+	["Sapphiron"] = false,
+	["Razorgore the Untamed"] = false,
+	["Onyxia"] = false,
+	["Princess Yauj"] = false,
+	["Vem"] = false,
+	["Shazzrah"] = false,
+	["Ouro"] = false,
+	["Battleguard Sartura"] = false,
+	["Maexxna"] = false,
+	["Kel'Thuzad"] = false,
+	["Nefarian"] = false,
+	["Fankriss the Unyielding"] = false,
+	["Lucifron"] = false,
+	["Anub'Rekhan"] = false,
+	["Hazza'rah"] = false,
+	["Ragnaros"] = false,
+	["Ayamiss the Hunter"] = false,
+	["Broodlord Lashlayer"] = false,
+	["Viscidus"] = false,
+	["Hakkar"] = false,
+	["Ebonroc"] = false,
+	["Kurinnaxx"] = false,
+	["Grobbulus"] = false,
+	["Instructor Razuvious"] = false,
+	["Emeriss"] = false,
+	["Magmadar"] = false,
+	["Sir Zeliek"] = false,
+	["Vaelastrasz the Corrupt"] = false,
+	["Patchwerk"] = false,
+	["Bloodlord Mandokir"] = false,
+	["Renataki"] = false,
+	["Jin'do the Hexxer"] = false,
+	["Garr"] = false,
+}
+EPVALS = {
+	["High Priestess Jeklik"] = "0",
+	["Gothik the Harvester"] = "0",
+	["Buru the Gorger"] = "0",
+	["Wushoolay"] = "0",
+	["Lord Kri"] = "0",
+	["Emperor Vek'nilash"] = "0",
+	["Ossirian the Unscarred"] = "0",
+	["Golemagg the Incinerator"] = "0",
+	["Grand Widow Faerlina"] = "0",
+	["High Priestess Mar'li"] = "0",
+	["Majordomo Executus"] = "0",
+	["High Priest Venoxis"] = "0",
+	["Gehennas"] = "0",
+	["Taerar"] = "0",
+	["Ysondre"] = "0",
+	["Thane Korth'azz"] = "0",
+	["High Priest Thekal"] = "0",
+	["Noth the Plaguebringer"] = "0",
+	["Lethon"] = "0",
+	["Moam"] = "0",
+	["The Prophet Skeram"] = "0",
+	["Flamegor"] = "0",
+	["C'Thun"] = "0",
+	["General Rajaxx"] = "0",
+	["Princess Huhuran"] = "0",
+	["Thaddius"] = "0",
+	["Loatheb"] = "0",
+	["Lady Blaumeux"] = "0",
+	["Highlord Mograine"] = "0",
+	["Heigan the Unclean"] = "0",
+	["Azuregos"] = "0",
+	["Doom Lord Kazzak"] = "0",
+	["Teremus the Devourer"] = "0",
+	["Chromaggus"] = "0",
+	["Baron Geddon"] = "0",
+	["Firemaw"] = "0",
+	["Sulfuron Harbinger"] = "0",
+	["Gri'lek"] = "0",
+	["High Priestess Arlokk"] = "0",
+	["Emperor Vek'lor"] = "0",
+	["Gahz'ranka"] = "0",
+	["Gluth"] = "0",
+	["Sapphiron"] = "0",
+	["Razorgore the Untamed"] = "0",
+	["Onyxia"] = "0",
+	["Princess Yauj"] = "0",
+	["Vem"] = "0",
+	["Shazzrah"] = "0",
+	["Ouro"] = "0",
+	["Battleguard Sartura"] = "0",
+	["Maexxna"] = "0",
+	["Kel'Thuzad"] = "0",
+	["Nefarian"] = "0",
+	["Fankriss the Unyielding"] = "0",
+	["Lucifron"] = "0",
+	["Anub'Rekhan"] = "0",
+	["Hazza'rah"] = "0",
+	["Ragnaros"] = "0",
+	["Ayamiss the Hunter"] = "0",
+	["Broodlord Lashlayer"] = "0",
+	["Viscidus"] = "0",
+	["Hakkar"] = "0",
+	["Ebonroc"] = "0",
+	["Kurinnaxx"] = "0",
+	["Grobbulus"] = "0",
+	["Instructor Razuvious"] = "0",
+	["Emeriss"] = "0",
+	["Magmadar"] = "0",
+	["Sir Zeliek"] = "0",
+	["Vaelastrasz the Corrupt"] = "0",
+	["Patchwerk"] = "0",
+	["Bloodlord Mandokir"] = "0",
+	["Renataki"] = "0",
+	["Jin'do the Hexxer"] = "0",
+	["Garr"] = "0",
+}
+BASEGP = 1
+STANDBYEP = false
+STANDBYOFFLINE = false
+STANDBYPERCENT = 0
+STANDBYRANKS = {
+	{
+		"Guild Master", -- [1]
+		false, -- [2]
+	}, -- [1]
+	{
+		"Officer", -- [1]
+		false, -- [2]
+	}, -- [2]
+	{
+		"Core", -- [1]
+		false, -- [2]
+	}, -- [3]
+	{
+		"Raider", -- [1]
+		false, -- [2]
+	}, -- [4]
+	{
+		"Member", -- [1]
+		false, -- [2]
+	}, -- [5]
+	{
+		"Alt", -- [1]
+		false, -- [2]
+	}, -- [6]
+	{
+		"Trial", -- [1]
+		false, -- [2]
+	}, -- [7]
+	{
+		"", -- [1]
+		false, -- [2]
+	}, -- [8]
+	{
+		"", -- [1]
+		false, -- [2]
+	}, -- [9]
+	{
+		"", -- [1]
+		false, -- [2]
+	}, -- [10]
+}
+SLOTWEIGHTS = {
+	["2HWEAPON"] = 0,
+	["NECK"] = 0,
+	["HEAD"] = 0,
+	["WEAPON"] = 1.5,
+	["WRIST"] = 0,
+	["WEAPONMAINHAND"] = 0,
+	["SHIELD"] = 0,
+	["HOLDABLE"] = 0,
+	["RANGED"] = 0,
+	["SHOULDER"] = 0,
+	["FEET"] = 0,
+	["LEGS"] = 0,
+	["EXCEPTION"] = 1,
+	["WAIST"] = 0,
+	["TRINKET"] = 0,
+	["HAND"] = 0,
+	["ROBE"] = 1,
+	["CHEST"] = 0,
+	["CLOAK"] = 0,
+	["FINGER"] = 0,
+	["WEAPONOFFHAND"] = 0,
+	["RANGEDRIGHT"] = 0,
+	["RELIC"] = 0,
+	["THROWN"] = 0,
+}
+RECORDS = {
+}
+OVERRIDE_INDEX = {
+	["Blastershot Launcher"] = 24,
+	["Earthfury Legguards"] = 18,
+	["Stormrage Cover"] = 22,
+	["Talisman of Ephemeral Power"] = 52,
+	["Sabatons of Might"] = 18,
+	["Robe of Volatile Power"] = 40,
+	["Felheart Slippers"] = 18,
+	["Arcanist Robes"] = 12,
+	["Crimson Shocker"] = 4,
+	["Salamander Scale Pants"] = 60,
+	["Onslaught Girdle"] = 80,
+	["Pants of Prophecy"] = 18,
+	["Pauldrons of Might"] = 18,
+	["Helm of the Lifegiver"] = 8,
+	["Giantstalker's Gloves"] = 18,
+	["Magma Tempered Boots"] = 0,
+	["Boots of Prophecy"] = 18,
+	["Aged Core Leather Gloves"] = 24,
+	["Nightslayer Belt"] = 10,
+	["Fireproof Cloak"] = 4,
+	["Eskhander's Collar"] = 16,
+	["Azuresong Mageblade"] = 28,
+	["Nightslayer Boots"] = 18,
+	["Dragon's Blood Cape"] = 4,
+	["Belt of Might"] = 10,
+	["Giantstalker's Bracers"] = 10,
+	["Nightslayer Pants"] = 18,
+	["Bindings of the Windseeker"] = 150,
+	["Earthfury Bracers"] = 10,
+	["The Eye of Divinity"] = 36,
+	["Vambraces of Prophecy"] = 10,
+	["Sabatons of the Flamewalker"] = 0,
+	["Aurastone Hammer"] = 24,
+	["Bloodfang Pants"] = 22,
+	["Leggings of Transcendence"] = 22,
+	["Felheart Pants"] = 18,
+	["Gloves of Prophecy"] = 18,
+	["Essence of the Pure Flame"] = 4,
+	["Quick Strike Ring"] = 68,
+	["Earthfury Boots"] = 18,
+	["Earthfury Helmet"] = 18,
+	["Helm of Might"] = 18,
+	["Ring of Binding"] = 12,
+	["Tome of Tranquilizing Shot"] = 0,
+	["Heavy Dark Iron Ring"] = 28,
+	["Manastorm Leggings"] = 8,
+	["Arcanist Bindings"] = 10,
+	["Arcanist Mantle"] = 18,
+	["Netherwind Crown"] = 22,
+	["Head of Onyxia"] = 48,
+	["Fireguard Shoulders"] = 4,
+	["Earthfury Vestments"] = 12,
+	["Gloves of the Hypnotic Flame"] = 16,
+	["Gutgore Ripper"] = 12,
+	["Cenarion Gloves"] = 18,
+	["Felheart Bracers"] = 10,
+	["Felheart Gloves"] = 18,
+	["Choker of Enlightenment"] = 8,
+	["Nightslayer Shoulder Pads"] = 18,
+	["Eskhandar's Right Claw"] = 16,
+	["Dragonstalker's Helm"] = 22,
+	["Giantstalker's Boots"] = 18,
+	["Ancient Petrified Leaf"] = 52,
+	["Fire Runed Grimoire"] = 12,
+	["Giantstalker's Breastplate"] = 12,
+	["Eye of Sulfuras"] = 150,
+	["Nemesis Skullcap"] = 22,
+	["Cenarion Spaulders"] = 18,
+	["Cenarion Helm"] = 18,
+	["Cenarion Leggings"] = 18,
+	["Giantstalker's Belt"] = 10,
+	["Legplates of Might"] = 18,
+	["Legplates of Ten Storms"] = 22,
+	["Shard of the Scale"] = 60,
+	["Mantle of Prophecy"] = 18,
+	["Beastplate of Might"] = 12,
+	["Striker's Mark"] = 60,
+	["Bloodfang Hood"] = 22,
+	["Earthfury Belt"] = 10,
+	["Giantstalker's Leggings"] = 18,
+	["Wristguards of True Flight"] = 36,
+	["Arcanist Crown"] = 18,
+	["Arcanist Leggings"] = 18,
+	["Wristguards of Stability"] = 8,
+	["Finkle's Lava Dredger"] = 12,
+	["Vis'kag the Bloodletter"] = 48,
+	["Choker of the Firelord"] = 60,
+	["Flameguard Gauntlets"] = 14,
+	["Gauntlets of Might"] = 18,
+	["Stormrage Legguards"] = 22,
+	["Deathbringer"] = 52,
+	["Staff of Dominance"] = 28,
+	["Spinal Reaper"] = 8,
+	["Nemesis Leggings"] = 22,
+	["Felheart Belt"] = 10,
+	["Arcanist Gloves"] = 18,
+	["Flamewalker Legplates"] = 4,
+	["Bracers of Might"] = 10,
+	["Earthshaker"] = 0,
+	["Arcanist Belt"] = 10,
+	["Earthfury Gauntlets"] = 18,
+	["Brutality Blade"] = 28,
+	["Sapphiron Drape"] = 20,
+	["Arcanist Boots"] = 18,
+	["Sash of Whispered Secrets"] = 16,
+	["Ring of Spell Power"] = 36,
+	["Band of Sulfuras"] = 12,
+	["Earthfury Epaulets"] = 18,
+	["Perdition's Blade"] = 56,
+	["Obsidian Edged Blade"] = 12,
+	["Nightslayer Gloves"] = 18,
+	["Nightslayer Cover"] = 18,
+	["Cenarion Bracers"] = 10,
+	["Nightslayer Chestpiece"] = 12,
+	["Legplates of Wrath"] = 22,
+	["Crown of Destruction"] = 56,
+	["Drillborer Disk"] = 28,
+	["Cenarion Vestments"] = 12,
+	["Core Hound Tooth"] = 44,
+	["Circlet of Prophecy"] = 18,
+	["Felheart Robes"] = 12,
+	["Cenarion Boots"] = 18,
+	["Cauterizing Band"] = 36,
+	["Nightslayer Bracelets"] = 10,
+	["Netherwind Pants"] = 22,
+	["Medallion of Steadfast Might"] = 48,
+	["Mature Black Dragon Sinew"] = 0,
+	["Mana Igniting Cord"] = 36,
+	["Malistar's Defender"] = 16,
+	["Helmet of Ten Storms"] = 22,
+	["Helm of Wrath"] = 22,
+	["Halo of Transcendence"] = 22,
+	["Girdle of Prophecy"] = 10,
+	["Giantstalker's Helmet"] = 18,
+	["Shard of the Flame"] = 4,
+	["Sorcerous Dagger"] = 16,
+	["Wild Growth Spaulders"] = 48,
+	["Bonereaver's Edge"] = 80,
+	["Felheart Shoulder Pads"] = 18,
+	["Felheart Horns"] = 18,
+	["Giantstalker's Epaulets"] = 18,
+	["Ancient Cornerstone Grimoire"] = 12,
+	["Dragonstalker's Legguards"] = 22,
+	["Deep Earth Spaulders"] = 0,
+	["Core Forged Geaves"] = 4,
+	["Cloak of the Shrouded Mists"] = 20,
+	["Cenarion Belt"] = 10,
+	["Robes of Prophecy"] = 12,
+	["Band of Accuria"] = 68,
+}
+TRAFFIC = {
+	{
+		"Drecken", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -12 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		93, -- [6]
+		81, -- [7]
+	}, -- [1]
+	{
+		"Aagrim", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -10 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		29, -- [6]
+		19, -- [7]
+	}, -- [2]
+	{
+		"Drecken", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -10 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		81, -- [6]
+		71, -- [7]
+	}, -- [3]
+	{
+		"Vidange", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -3 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		22, -- [6]
+		19, -- [7]
+	}, -- [4]
+	{
+		"Manon", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -3 (Fixing 10/13/2019)", -- [3]
+		120, -- [4]
+		120, -- [5]
+		22, -- [6]
+		19, -- [7]
+	}, -- [5]
+	{
+		"Drakoh", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -3 (Fixing 10/13/2019)", -- [3]
+		130, -- [4]
+		130, -- [5]
+		44, -- [6]
+		41, -- [7]
+	}, -- [6]
+	{
+		"Chimble", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -3 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		36, -- [6]
+		33, -- [7]
+	}, -- [7]
+	{
+		"Boundless", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -16 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		79, -- [6]
+		63, -- [7]
+	}, -- [8]
+	{
+		"Cidolbones", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -16 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		44, -- [6]
+		28, -- [7]
+	}, -- [9]
+	{
+		"Macewindu", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -12 (Fixing 10/13/2019)", -- [3]
+		50, -- [4]
+		50, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [10]
+	{
+		"Cidolbones", -- [1]
+		"Echohack", -- [2]
+		"Add GP +12 (Fixing uncharged T1 Chest)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		28, -- [6]
+		40, -- [7]
+	}, -- [11]
+	{
+		"Vyrze", -- [1]
+		"Echohack", -- [2]
+		"Add GP +48 (Fixing 10/13/2019 - Head of Onyxia)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		24, -- [6]
+		72, -- [7]
+	}, -- [12]
+	{
+		"Vyrze", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -23 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		72, -- [6]
+		49, -- [7]
+	}, -- [13]
+	{
+		"Cidolbones", -- [1]
+		"Echohack", -- [2]
+		"Add GP +1 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		140, -- [5]
+		40, -- [6]
+		41, -- [7]
+	}, -- [14]
+	{
+		"Frazetta", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		23, -- [6]
+		23, -- [7]
+	}, -- [15]
+	{
+		"Xpace", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		58, -- [6]
+		58, -- [7]
+	}, -- [16]
+	{
+		"Sissy", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		93, -- [6]
+		93, -- [7]
+	}, -- [17]
+	{
+		"Couillon", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [18]
+	{
+		"Zatodar", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		109, -- [6]
+		109, -- [7]
+	}, -- [19]
+	{
+		"Vwins", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		25, -- [6]
+		25, -- [7]
+	}, -- [20]
+	{
+		"Vidange", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		19, -- [6]
+		19, -- [7]
+	}, -- [21]
+	{
+		"Shizz", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [22]
+	{
+		"Drecken", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		71, -- [6]
+		71, -- [7]
+	}, -- [23]
+	{
+		"Chimble", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		33, -- [6]
+		33, -- [7]
+	}, -- [24]
+	{
+		"Boundless", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		63, -- [6]
+		63, -- [7]
+	}, -- [25]
+	{
+		"Vyrze", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		49, -- [6]
+		49, -- [7]
+	}, -- [26]
+	{
+		"Cidolbones", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		41, -- [6]
+		41, -- [7]
+	}, -- [27]
+	{
+		"Aagrim", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		140, -- [4]
+		190, -- [5]
+		19, -- [6]
+		19, -- [7]
+	}, -- [28]
+	{
+		"Crystalist", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		130, -- [4]
+		180, -- [5]
+		73, -- [6]
+		73, -- [7]
+	}, -- [29]
+	{
+		"Gigz", -- [1]
+		"Echohack", -- [2]
+		"Add EP +30 (Fixing 10/13/2019)", -- [3]
+		130, -- [4]
+		160, -- [5]
+		11, -- [6]
+		11, -- [7]
+	}, -- [30]
+	{
+		"Drakoh", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		130, -- [4]
+		180, -- [5]
+		41, -- [6]
+		41, -- [7]
+	}, -- [31]
+	{
+		"Scorched", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		120, -- [4]
+		170, -- [5]
+		29, -- [6]
+		29, -- [7]
+	}, -- [32]
+	{
+		"Manon", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		120, -- [4]
+		170, -- [5]
+		19, -- [6]
+		19, -- [7]
+	}, -- [33]
+	{
+		"Cracktown", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		115, -- [4]
+		165, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [34]
+	{
+		"Cowhileonard", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		115, -- [4]
+		165, -- [5]
+		29, -- [6]
+		29, -- [7]
+	}, -- [35]
+	{
+		"Hungrysalami", -- [1]
+		"Echohack", -- [2]
+		"Add EP +15 (Fixing 10/13/2019)", -- [3]
+		95, -- [4]
+		110, -- [5]
+		13, -- [6]
+		13, -- [7]
+	}, -- [36]
+	{
+		"Cowhileonard", -- [1]
+		"Echohack", -- [2]
+		"Subtract EP -5 (Fixing 10/13/2019)", -- [3]
+		165, -- [4]
+		160, -- [5]
+		29, -- [6]
+		29, -- [7]
+	}, -- [37]
+	{
+		"Cracktown", -- [1]
+		"Echohack", -- [2]
+		"Subtract EP -5 (Fixing 10/13/2019)", -- [3]
+		165, -- [4]
+		160, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [38]
+	{
+		"Meshona", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		80, -- [4]
+		130, -- [5]
+		17, -- [6]
+		17, -- [7]
+	}, -- [39]
+	{
+		"Kagedorf", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		60, -- [4]
+		110, -- [5]
+		49, -- [6]
+		49, -- [7]
+	}, -- [40]
+	{
+		"Macewindu", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		50, -- [4]
+		100, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [41]
+	{
+		"Zolya", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		50, -- [4]
+		100, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [42]
+	{
+		"Krusty", -- [1]
+		"Echohack", -- [2]
+		"Add EP +50 (Fixing 10/13/2019)", -- [3]
+		40, -- [4]
+		90, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [43]
+	{
+		"Echohack", -- [1]
+		"Echohack", -- [2]
+		"Add EP +30 (Fixing 10/13/2019)", -- [3]
+		110, -- [4]
+		140, -- [5]
+		19, -- [6]
+		19, -- [7]
+	}, -- [44]
+	{
+		"Servicer", -- [1]
+		"Echohack", -- [2]
+		"Add EP +40 (Fixing 10/13/2019)", -- [3]
+		10, -- [4]
+		50, -- [5]
+		1, -- [6]
+		1, -- [7]
+	}, -- [45]
+	{
+		"Vwins", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -8 (Overcharged for sash of whispered secrets)", -- [3]
+		190, -- [4]
+		190, -- [5]
+		25, -- [6]
+		17, -- [7]
+	}, -- [46]
+	{
+		"Chimble", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -4 (fixing 10/13/2019)", -- [3]
+		190, -- [4]
+		190, -- [5]
+		33, -- [6]
+		29, -- [7]
+	}, -- [47]
+	{
+		"Scorched", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -10 (Fixing 10/13/2019)", -- [3]
+		170, -- [4]
+		170, -- [5]
+		29, -- [6]
+		19, -- [7]
+	}, -- [48]
+	{
+		"Xpace", -- [1]
+		"Echohack", -- [2]
+		"Subtract GP -17 (Fixing 10/13/2019)", -- [3]
+		190, -- [4]
+		190, -- [5]
+		58, -- [6]
+		41, -- [7]
+	}, -- [49]
+}
+CEPGP_raid_logs = {
+}
+CEPGP_standby_accept_whispers = false
+CEPGP_standby_byrank = true
+CEPGP_standby_manual = false
+CEPGP_standby_whisper_msg = "standby"
+CEPGP_notice = true
+CEPGP_keyword = "!need"
+ALLOW_FORCED_SYNC = true
+CEPGP_force_sync_rank = 1
+CEPGP_loot_GUI = false
+CEPGP_GP_decs = "0"
+CEPGP_auto_pass = false
+CEPGP_raid_wide_dist = false
+CEPGP_standby_share = false
+CEPGP_1120_notice = true
